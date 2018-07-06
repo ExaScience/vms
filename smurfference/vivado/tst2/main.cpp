16707c93ea1c5372bdfd9710dd768f9df2222e76
Tom Vander Aa <vanderaa@imec.be>
2018-07-06 16:33:27 +0200

    using hls for matmul

--- smurfference/vivado/tst2/main.cpp
+++ smurfference/vivado/tst2/main.cpp
@@ -1,38 +1,9 @@
 #include <iostream>
 
-#include <Eigen/Dense>
+#include "hls/linear_algebra/utils/x_hls_matrix_utils.h"
+#include "hls/linear_algebra/utils/x_hls_matrix_tb_utils.h"
 
 #include "predict_tst2.h"
-
-typedef const Eigen::Matrix<double, num_latent, num_feat, Eigen::RowMajor> MatrixF0;
-typedef const Eigen::Matrix<double, num_latent, num_proteins, Eigen::RowMajor> MatrixU1;
-typedef const Eigen::Matrix<double, num_comp, num_feat, Eigen::RowMajor> MatrixIn;
-typedef       Eigen::Matrix<double, num_comp, num_proteins, Eigen::RowMajor> MatrixOut;
-
-typedef Eigen::Map<MatrixF0> MapF0;
-typedef Eigen::Map<MatrixU1> MapU1;
-typedef Eigen::Map<MatrixIn> MapIn;
-typedef Eigen::Map<MatrixOut> MapOut;
-
-
-void predict_compound_block_eigen(
-   const double in[num_comp][num_feat],
-   double out[num_comp][num_proteins]
-)
-{
-   MapF0  map_f0 (&F0 [0][0]);
-   MapU1  map_u1 (&U1 [0][0]);
-   MapIn  map_in (&in [0][0]);
-   MapOut map_out(&out[0][0]);
-
-   //        ((nc x nf) * (nf * nl)) * (nl * np)
-   //        (nc        x        nl) * (nl * np)
-   //        (nc             x               np)
-   map_out = (map_in * map_f0.transpose()) * map_u1;
-
-   std::cout << __func__ << ":\n" << map_out << std::endl;
-}
-
 
 int main()
 {
@@ -47,16 +18,34 @@ int main()
         {0.96, 1.29},
     };
 
-    double output_pred1[num_comp][num_proteins];
+
+    double output_pred1[num_comp][num_proteins] = {
+    		{ 2.08209 , 1.97539 , 2.27278},
+			{ 6.16534 , 5.80551 , 6.66045},
+			{ 0.560084, 0.521615, 0.595898},
+			{ 6.10232 , 5.85359 , 6.76267},
+			{ 2.08209 , 1.97539 , 2.27278},
+			{ 6.16534 , 5.80551 , 6.66045},
+			{ 0.560084, 0.521615, 0.595898},
+			{ 6.10232 , 5.85359 , 6.76267},
+    };
+
     double output_pred2[num_comp][num_proteins];
 
-    predict_compound_block_eigen(input_features, output_pred1);
     predict_compound_block_c(input_features, output_pred2);
 
-    MapOut map_out1(&output_pred1[0][0]);
-    MapOut map_out2(&output_pred2[0][0]);
+    printf("output = \n");
+    hls::print_matrix<num_comp, num_proteins, double, hls::NoTranspose>(output_pred2, "   ");
 
-    std::cout << "diff: " << (map_out1 - map_out2).norm() << std::endl;
+    // Generate error ratio and compare against threshold value
+    // - LAPACK error measurement method
+    // - Threshold taken from LAPACK test functions
+    if ( difference_ratio<num_comp, num_proteins>(output_pred1,output_pred2) > 30.0 ) {
+      printf("  --> error\n");
+      return(1);
+    }
+    return(0);
+
 
     return 0;
 }
--- smurfference/vivado/tst2/vivado_hls.app
+++ smurfference/vivado/tst2/vivado_hls.app
@@ -1,14 +1,14 @@
 <project xmlns="com.autoesl.autopilot.project" name="tst2" top="predict_compound_block_c">
-    <includePaths/>
-    <libraryPaths/>
-    <Simulation>
-        <SimFlow name="csim" setup="true" csimMode="0" lastCsimMode="0" compiler="true"/>
-    </Simulation>
-    <files xmlns="">
-        <file name="../main.cpp" sc="0" tb="1" cflags=" -I/opt/software/Eigen/3.3.4/include "/>
+    <files>
+        <file name="../main.cpp" sc="0" tb="1" cflags=" -I../../../../../../../../opt/software/Eigen/3.3.4/include "/>
         <file name="tst2/predict_tst2.h" sc="0" tb="false" cflags=""/>
         <file name="tst2/predict_tst2.cpp" sc="0" tb="false" cflags=""/>
     </files>
+    <includePaths/>
+    <libraryPaths/>
+    <Simulation argv="">
+        <SimFlow name="csim" ldflags="" mflags="" csimMode="0" lastCsimMode="0" compiler="true"/>
+    </Simulation>
     <solutions xmlns="">
         <solution name="solution1" status="active"/>
     </solutions>
