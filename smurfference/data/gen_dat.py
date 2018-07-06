6f632ecbeed44a2b08a85c9f9e07267c4fccc6f9
Tom Vander Aa <vanderaa@imec.be>
2018-07-06 11:26:06 +0200

    gen_dat updates

--- smurfference/data/gen_dat.py
+++ smurfference/data/gen_dat.py
@@ -4,23 +4,32 @@ import matrix_io as mio
 import numpy as np
 import sys
 import os.path as pth
+import io
 
 def gen_dat(M, typename, varname):
-    h = typename + " " + varname + "[%d][%d] = {" % M.shape
-    f = "};"
+    hdr = "const int %s_rows = %d;\n" % (varname, M.shape[0])
+    hdr += "const int %s_cols = %d;\n" % (varname, M.shape[1])
+    hdr += typename + " " + varname + "[%d][%d] = {" % M.shape
+    ftr = "};"
+    fmt = "    { " + "%.2f, " * (M.shape[1] - 1) + "%.2f },"
 
-    fmt = "{ " + "%.2f, " * (M.shape[1] - 1) + "%.2f }"
+    f = io.StringIO() 
 
-    np.savetxt(sys.stdout, M,
+    np.savetxt(f, M,
             fmt = fmt,
-            header = h,
-            footer = f,
-            newline = "\n    ",
-            comments = "");
+            header = hdr,
+            footer = ftr,
+            newline = "\n",
+            comments = "")
+
+    ret = f.getvalue()
+
+    return ret
 
 # main
 for f in sys.argv[1:]:
     M = mio.read_matrix(f)
     varname = pth.basename(pth.splitext(f)[0]).replace("-", "_")
-    gen_dat(M, "double", varname)
+    s = gen_dat(M, "const double", varname)
+    print(s)
 
