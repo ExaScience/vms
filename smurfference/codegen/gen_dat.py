3c14dd924afb9ec502331a8669085ce777480cd4
Tom Vander Aa <vanderaa@imec.be>
2018-07-10 14:40:48 +0200

    Update codegen

--- smurfference/codegen/gen_dat.py
+++ smurfference/codegen/gen_dat.py
@@ -8,6 +8,20 @@ import os.path as pth
 import io
 import argparse
 
+def parse_selection(str):
+    s = str.split("-")
+    if (str == "all"):
+        selection = (0, 9999)
+    elif (len(s) == 1):
+        selection = (int(s[0]), int(s[0]))
+    elif (len(s) == 2):
+        if s[0] == "": s[0] = 0
+        if s[1] == "": s[1] = 9999
+        selection = ( int(s[0]), int(s[1]))
+    else:
+        raise ValueError("Could not parse sample selection: " + args.samples)
+
+    return selection
 
 def gen_int(name, value):
     """Generates code for an int:
@@ -50,10 +64,7 @@ const double sample_1_U0_latents[4][3] = {
 
     return f.getvalue()
 
-def gen_sample(sample, selection):
-    if (sample.iter < selection[0] or sample.iter > selection[1]):
-        return "// skip sample %d\n" % sample.iter
-
+def gen_sample(sample):
     ret = ""
     for i, U in enumerate(sample.latents):
         ret += gen_mat(U, "double", "s%d_U%d" % (sample.iter, i))
@@ -65,41 +76,55 @@ def gen_sample(sample, selection):
 parser = argparse.ArgumentParser(description='Generate SMURFF HLS inferencer C-code')
 parser.add_argument('root', metavar='root-file', type=str, help='root file')
 parser.add_argument('--selection', dest='selection', default='all', help='sample or samples to include (e.g. 1, 2-10, all)')
-parser.add_argument('--output', dest='output', default='smurff_model.h', help='output file')
+parser.add_argument('--model-out', dest='model_out', default='smurff_model.h', help='model output file')
+
+
+parser.add_argument('--testbench', dest='tb_in', default=None, help='testbench input file')
+parser.add_argument('--testbench-out', dest='tb_out', default='smurff_tb.h', help='testbench output file')
 args = parser.parse_args()
 
-def parse_selection(str):
-    s = str.split("-")
-    if (str == "all"):
-        selection = (0, 9999)
-    elif (len(s) == 1):
-        selection = (int(s[0]), int(s[0]))
-    elif (len(s) == 2):
-        if s[0] == "": s[0] = 0
-        if s[1] == "": s[1] = 9999
-        selection = ( int(s[0]), int(s[1]))
-    else:
-        raise ValueError("Could not parse sample selection: " + args.samples)
-
-    return selection
 
 selection = parse_selection(args.selection)
 session = smurff.PredictSession.fromRootFile(args.root)
 
+num_latent = session.num_latent()
+num_comp, num_proteins = session.data_shape()
+num_feat = session.beta_shape()[0]
 
-output = ""
-output += gen_int("num_latent", session.num_latent())
-output += gen_int("num_comp", session.data_shape()[0])
-output += gen_int("num_proteins", session.data_shape()[1])
-output += gen_int("num_feat", session.beta_shape()[0])
+model_output = ""
+model_output += gen_int("num_latent", num_latent)
+model_output += gen_int("num_comp", num_comp)
+model_output += gen_int("num_proteins", num_proteins)
+model_output += gen_int("num_feat", num_feat)
 
+if (args.tb_in):
+    tb_output = ""
+    #generate testbench
+    tb_in_matrix = mio.read_matrix(args.tb_in)
+
+    (tb_num_comp, tb_num_feat) = tb_in_matrix.shape
+
+    assert tb_num_feat == num_feat
+    tb_output += gen_int("tb_num_comp", tb_num_comp)
+    tb_output += gen_mat(tb_in_matrix, "double", "tb_input")
+
+#generate model
 for sample in session.samples:
-    output += gen_sample(sample, selection)
+    if (sample.iter < selection[0] or sample.iter > selection[1]):
+        model_output += "// skip sample %d\n" % sample.iter
+        tb_output += "// skip sample %d\n" % sample.iter
 
-with open(args.output, "w") as os:
-    os.write(output)
+    model_output += gen_sample(sample)
 
+    if (args.tb_in):
+        predictions = []
+        np.apply_along_axis( lambda f: predictions.append(sample.predict((f, None))), axis=1, arr=tb_in_matrix )
+        tb_ref_matrix = np.stack(predictions)
+        tb_output += gen_mat(tb_ref_matrix, "double", "s%d_tb_ref" % sample.iter)
 
+with open(args.model_out, "w") as os:
+    os.write(model_output)
 
-
+with open(args.tb_out, "w") as os:
+    os.write(tb_output)
 
