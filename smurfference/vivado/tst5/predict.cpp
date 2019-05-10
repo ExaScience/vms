6b0a8bf3e98ac4b5003fb1f1662d45dd5304c5f2
Tom Vander Aa <vanderaa@imec.be>
2019-05-10 11:28:31 +0200

    working csynth predict.cpp

--- smurfference/vivado/tst5/predict.cpp
+++ smurfference/vivado/tst5/predict.cpp
@@ -17,40 +17,41 @@ void predict_compound_block_c(
 
     int c, d, k;
     U_type tmp[num_latent];
-    //#pragma HLS ARRAY_PARTITION variable=tmp complete dim=1
+#pragma HLS ARRAY_PARTITION variable=tmp complete dim=1
 
     F_type in_buf[num_features];
+#pragma HLS ARRAY_PARTITION variable=in_buf complete dim=1
     S_type out_buf[num_proteins];
-    //#pragma HLS ARRAY_PARTITION variable=in_buf complete dim=1
+#pragma HLS ARRAY_PARTITION variable=out_buf complete dim=1
 
 //       ((nc x nf) * (nf * nl)) * (nl * np)
 //       (nc        x        nl) * (nl * np)
 //       (nc             x               np)
 // out = ((in * f0.transpose()) + mu) * u1;
 
-// tmp = in * F0
 predict_compound_block_c_loop11:
     for (c = 0; c < num_compounds; c++)
     {
-#pragma HLS PIPELINE II = 1
-
         for (k = 0; k < num_features; k++)
         {
+#pragma HLS UNROLL
             in_buf[k] = in[c][k];
         }
 
         for (d = 0; d < num_proteins; d++)
         {
+#pragma HLS UNROLL
             out_buf[d] = .0;
         }
 
         for (int s = 0; s < num_samples; s++)
         {
-        predict_compound_block_c_loop12:
             for (d = 0; d < num_latent; d++)
             {
+#pragma HLS PIPELINE II=1
+#pragma HLS ARRAY_PARTITION variable=B complete dim=3
+
                 U_type sum = .0;
-            predict_compound_block_c_loop13:
                 for (k = 0; k < num_features; k++)
                 {
                     sum = sum + in_buf[k] * B[s][d][k];
@@ -59,23 +60,22 @@ predict_compound_block_c_loop11:
                 tmp[d] = sum + mu[s][d];
             }
 
-        predict_compound_block_c_loop22:
             for (d = 0; d < num_proteins; d++)
             {
+#pragma HLS PIPELINE II=1
                 S_type sum = .0;
-            predict_compound_block_c_loop23:
                 for (k = 0; k < num_latent; k++)
                 {
                     sum = sum + tmp[k] * U[s][k][d];
                 }
 
                 out_buf[d] += sum;
-
             } // end proteins
         } // end samples
 
         for (d = 0; d < num_proteins; d++)
         {
+#pragma HLS UNROLL
             out[c][d] = out_buf[d] / (S_type)num_samples;
         }
     } // end compounds
