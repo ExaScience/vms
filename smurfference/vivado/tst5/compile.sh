decc406447969096ab1ba86b616ac3436a6b1e60
Tom Vander Aa <vanderaa@imec.be>
2019-05-10 11:48:24 +0200

    Cleanup

--- smurfference/vivado/tst5/compile.sh
+++ smurfference/vivado/tst5/compile.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-XILINX_VIVADO_HLS=/home/vanderaa/Xilinx/Vivado_HLS/2017.2
-
 set -x 
-g++ -g -O0 -I$XILINX_VIVADO_HLS/include *.cpp -o run \
+g++ -g -O0 -I$XILINX_VIVADO/include *.cpp -o run \
 	-L /usr/local/opt/systemc/lib -lsystemc
