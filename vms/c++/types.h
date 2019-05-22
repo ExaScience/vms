4b4e2ef1f6c867b737bccb63d3398be037a06000
Tom Vander Aa <vanderaa@imec.be>
2019-05-22 22:28:19 +0200

    FIX: more integer bits for P_type after prev commit

--- vms/c++/types.h
+++ vms/c++/types.h
@@ -23,20 +23,11 @@ const float epsilon = 0.0001;
 
 #define DT_NAME "fixed"
 
-/*
-typedef sc_fixed<16,6>  U_type;
-typedef sc_fixed<16,4>  mu_type;
-typedef sc_fixed<16,5>  F_type;
-typedef sc_fixed<16,5>  P_type;
-typedef sc_fixed<16,5>  B_type;
-typedef sc_fixed<20,9>  S_type;
-typedef sc_fixed<20,9>  T_type;
-*/
 typedef sc_fixed<24,6>  U_type;
 typedef sc_fixed<8,1>  mu_type;
 typedef sc_fixed<20,1>  B_type;
 typedef sc_fixed<20,10>  F_type;
-typedef sc_fixed<16,5>  P_type;
+typedef sc_fixed<16,8>  P_type;
 typedef sc_fixed<32,10>  S_type;
 typedef float  T_type;
 
