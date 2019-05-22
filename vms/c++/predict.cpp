f21f0f75fa2da64fd0d54edd99fdfaf00bf4d2f2
Tom Vander Aa <vanderaa@imec.be>
2019-05-22 13:05:35 +0200

    conversion fix

--- vms/c++/predict.cpp
+++ vms/c++/predict.cpp
@@ -99,7 +99,7 @@ predict_compound_block_c_loop11:
         for (d = 0; d < num_proteins; d++)
         {
 #pragma HLS UNROLL
-            predictions[c][d] = (float)(out_buf[d] / (S_type)num_samples);
+            predictions[c][d] = out_buf[d] / (S_type)num_samples;
         }
     } // end compounds
 }
