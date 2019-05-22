46ae25e76bc66f20206b3a0de743fbfffd04bb39
Tom Vander Aa <vanderaa@imec.be>
2019-05-22 13:24:54 +0200

    ENH: tb input static

--- vms/c++/main.cpp
+++ vms/c++/main.cpp
@@ -33,26 +33,26 @@ int main()
     printf("  nlat:  %d\n", num_latent);
     printf("  nsmpl: %d\n", num_samples);
 
-    P_type    tb_output[num_compounds][num_proteins];
-    F_type  tb_input_fx[num_compounds][num_features];
-    U_type         U_fx[num_samples][num_proteins][num_latent];
-    mu_type       mu_fx[num_samples][num_latent];
-    B_type         B_fx[num_samples][num_latent][num_features];
-    
+    static P_type tb_output_fx[num_compounds][num_proteins];
+    static F_type  tb_input_fx[num_compounds][num_features];
+    static U_type         U_fx[num_samples][num_proteins][num_latent];
+    static mu_type       mu_fx[num_samples][num_latent];
+    static B_type         B_fx[num_samples][num_latent][num_features];
+
     CONVERT2(tb_input);
     CONVERT3(U);
     CONVERT2(mu);
     CONVERT3(B);
 
-    predict_compound_block_c(tb_input_fx, tb_output, U_fx, mu_fx, B_fx);
+    predict_compound_block_c(tb_input_fx, tb_output_fx, U_fx, mu_fx, B_fx);
 
     int nerrors = 0;
     for(int c=0; c<num_compounds; c++)
         for(int p=0; p<num_proteins; p++)
         {
-            float o = (float)tb_output[c][p];
+            float o = (float)tb_output_fx[c][p];
             float r = (float)tb_ref[c][p];
-            // printf("[%d][%d]: out is %f -- ref is %f\n", c, p, tb_output[c][p], tb_ref[c][p]);
+            // printf("[%d][%d]: out is %f -- ref is %f\n", c, p, tb_output_fx[c][p], tb_ref[c][p]);
             if (std::abs(o - r) > epsilon)
             {
                 printf("error at [%d][%d]: %f != %f\n", c, p, o, r);
