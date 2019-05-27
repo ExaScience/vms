d6fae4f0c2b9bc9cb2297427ab7183c46164a539
Tom Vander Aa <vanderaa@imec.be>
2019-05-27 11:16:52 +0200

    WIP: fxp struct experiment

--- vms/c++/test_fxp.cpp
+++ vms/c++/test_fxp.cpp
@@ -14,36 +14,68 @@ struct fxp
     int shift() const { return wl - iwl; }
 
 	float to_float() const {
-		return ((float)val) / (1<<shift());
+		return ((float)val) / (float)(1L<<shift());
 	}
 
     fxp(int i, float v = .0)
-        : val(v*(1<<shift())), iwl(i)
+        : iwl(i)
     {
-        assert(fabs(v) < (1<<(iwl-1)));
+        val = v*(1L<<shift()); 
+        assert(fabs(v) < (1L<<(iwl-1)));
     }
 
     fxp(int i, T v)
         : val(v), iwl(i)
     {
-        assert(fabs(v) < (1<<(wl-1)));
+        assert(fabs(v) < (1L<<(wl-1)));
     }
 
-    fxp<long long> operator*(const fxp<T> other) const
+    template<typename otherT>
+    fxp(int i, fxp<otherT> v)
+        : iwl(i)
     {
-        fxp<long long> ret(iwl + other.iwl);
-        ret.val = (val * other.val) >> (shift() + other.shift() - ret.shift());
+        mul_type temp_val = v.val >> (v.shift() - shift());
+        printf("temp_val = %lld\n", temp_val);
+        assert(abs(temp_val) < (1L<<(wl-1)));
+        val = temp_val;
+    }
+
+    typedef int mul_type;
+
+    fxp<mul_type> operator*(const fxp<T> other) const
+    {
+        fxp<mul_type> ret(iwl + other.iwl);
+
+        int s = shift() + other.shift() - ret.shift();
+        if (s > 0) ret.val = (mul_type)(val * other.val) >> s;
+        else     ret.val = (mul_type)(val * other.val) << -s;
         return ret;
+    }
+
+    void print(const char *name) const
+    {
+        printf("%s\n val = %.2f\n", name, to_float());
+        printf(" wl = %d\n", wl);
+        printf(" iwl = %d\n", iwl);
     }
 };
 
+#define SHOW(F) F.print(#F)
+
 int main()
 {
-    fxp<short> f1(10, 20.2F);
-    fxp<short> f2(4, 2.1F);
+    fxp<short> f1(7, 20.2F);
+    fxp<short> f2(5, 2.1F);
 
-    printf("f1 = %.2f\n", f1.to_float());
-    printf("f2 = %.2f\n", f2.to_float());
+    SHOW(f1);
+    SHOW(f2);
+
+    auto m = f1 * f2;
+    SHOW(m);
+
+    fxp<short> n(10, m);
+    SHOW(n);
+
 
     return 0;
 
