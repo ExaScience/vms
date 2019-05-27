20c81b5b73443379776945c828d493df1401a885
Tom Vander Aa <vanderaa@imec.be>
2019-05-27 11:18:08 +0200

    WIP: fxp struct experiment

---
+++ vms/c++/fxp.h
@@ -0,0 +1,63 @@
+
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+
+template<typename T>
+struct fxp
+{
+	static const int wl = sizeof(T) * 8;
+
+	T val;
+    int iwl;
+
+    int shift() const { return wl - iwl; }
+
+	float to_float() const {
+		return ((float)val) / (float)(1L<<shift());
+	}
+
+    fxp(int i, float v = .0)
+        : iwl(i)
+    {
+        val = v*(1L<<shift()); 
+        assert(fabs(v) < (1L<<(iwl-1)));
+    }
+
+    fxp(int i, T v)
+        : val(v), iwl(i)
+    {
+        assert(fabs(v) < (1L<<(wl-1)));
+    }
+
+    template<typename otherT>
+    fxp(int i, fxp<otherT> v)
+        : iwl(i)
+    {
+        mul_type temp_val = v.val >> (v.shift() - shift());
+        printf("temp_val = %lld\n", temp_val);
+        assert(abs(temp_val) < (1L<<(wl-1)));
+        val = temp_val;
+    }
+
+    typedef int mul_type;
+
+    fxp<mul_type> operator*(const fxp<T> other) const
+    {
+        fxp<mul_type> ret(iwl + other.iwl);
+
+        int s = shift() + other.shift() - ret.shift();
+        if (s > 0) ret.val = (mul_type)(val * other.val) >> s;
+        else     ret.val = (mul_type)(val * other.val) << -s;
+        return ret;
+    }
+
+    void print(const char *name) const
+    {
+        printf("%s\n val = %.2f\n", name, to_float());
+        printf(" wl = %d\n", wl);
+        printf(" iwl = %d\n", iwl);
+    }
+};
+
+#define SHOW(F) F.print(#F)
