9908eee21e1788403591d3b56e0a59c360adb7f3
Tom Vander Aa <vanderaa@imec.be>
2019-06-04 15:44:21 +0200

    Update top-level function name in vivado tcl script

--- vms/codegen/run.tcl
+++ vms/codegen/run.tcl
@@ -1,5 +1,5 @@
 open_project -reset hls
-set_top predict_compound_block_c
+set_top predict_or_update_model
 add_files code/predict.cpp -cflags "-DDT_FIXED"
 add_files -tb code/main.cpp -cflags "-DDT_FIXED"
 open_solution "solution1"
@@ -7,5 +7,5 @@ set_part {xczu9eg-ffvb1156-1l-i} -tool vivado
 create_clock -period 5 -name default
 csim_design -compiler clang
 csynth_design
-#cosim_design
-#export_design -rtl verilog -format ip_catalog
+cosim_design
+export_design -rtl verilog -format ip_catalog
