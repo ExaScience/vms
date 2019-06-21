004bf13232bea98db4aa8ed1f32381e8d8f01c14
Tom Vander Aa <vanderaa@imec.be>
2019-06-21 16:20:30 +0200

    Makefile fixes and OmpSS scripts

--- vms/ompss/random/fpga-zcu102/run.sh
+++ vms/ompss/random/fpga-zcu102/run.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/instrumentation
-export XTASKS_CONFIG_FILE=predict.xtasks.config
-#export NX_ARGS="--fpga-enable --verbose --instrumentation=extrae"
-export NX_ARGS="--instrumentation=extrae"
-export EXTRAE_CONFIG_FILE=extrae.xml
+export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/debug
+export XTASKS_CONFIG_FILE=zcu102/predict.xtasks.config
+export NX_ARGS="--fpga-enable --verbose"
+#export NX_ARGS="--instrumentation=extrae"
+export EXTRAE_CONFIG_FILE=zcu102/extrae.xml
 
-strace -o strace.log ./predict-i
+./predict-i
