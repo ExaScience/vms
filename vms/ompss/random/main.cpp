523569f99d9aa8d4251cf083c9a8de75093b7e8f
Tom Vander Aa <vanderaa@imec.be>
2019-06-21 16:44:08 +0200

    Added performance counting

--- vms/ompss/random/main.cpp
+++ vms/ompss/random/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cmath>
 #include <iostream>
+#include <chrono>
 
 #include "predict.h"
 #include "smurff_tb.h"
@@ -12,6 +13,12 @@ std::vector<float> values[ntypes];
 
 #define CONVERT2(N) convert(&(N[0][0]), &(N##_fx[0][0]), sizeof(N) / sizeof(N[0][0]))
 #define CONVERT3(N) convert(&(N[0][0][0]), &(N##_fx[0][0][0]), sizeof(N) / sizeof(N[0][0][0]))
+
+double tick() {
+    auto now = std::chrono::system_clock::now().time_since_epoch();
+    double ms = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+    return ms / 1000.;
+}
 
 template <typename F, typename T>
 void convert(const F *in, T *out, int size)
@@ -65,17 +72,25 @@ int main()
     CONVERT3(B);
 
     int nerrors = 0;
+    int num_repeat = 10000;
 
     printf("Updating model\n");
     update_model(U_fx, mu_fx, B_fx);
 
     printf("Predicting\n");
-    for(int c=0; c<num_compounds; c++)
+    double start = tick();
+    for(int r=0;r<num_repeat; r++)
     {
-        predict_compound(tb_input_fx[c], tb_output_fx[c]);
+        for(int c=0; c<num_compounds; c++)
+        {
+            predict_compound(tb_input_fx[c], tb_output_fx[c]);
+        }
     }
 #pragma omp taskwait
+    double stop = tick();
     nerrors += check_result(tb_output_fx, tb_ref);
+    double elapsed = stop-start;
+    printf("took %.2f sec; %.2f compounds/sec\n", elapsed, num_compounds * num_repeat / elapsed);
 
 #ifdef DT_OBSERVED_FLOAT
     for (int i = 0; i < ntypes; ++i)
