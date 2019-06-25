48b767abc45745c0e28472cab037d4f71abcbe04
Tom Vander Aa <vanderaa@imec.be>
2019-06-25 09:59:05 +0200

    ENH: check BOOT.bin

--- vms/ompss/random/fpga-zcu102/run.sh
+++ vms/ompss/random/fpga-zcu102/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 set -x
+set -e
 
-export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/debug
+cmp ./zcu102/predict_autoVivado/BOOT.BIN /boot/BOOT.BIN
+
+export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/performance
 export XTASKS_CONFIG_FILE=zcu102/predict.xtasks.config
-export NX_ARGS="--fpga-enable --verbose"
+#export NX_ARGS="--fpga-enable --verbose"
 #export NX_ARGS="--instrumentation=extrae"
 export EXTRAE_CONFIG_FILE=zcu102/extrae.xml
 
