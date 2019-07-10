81308bdfbc840ed614fc8472618bfa1a50232a52
Tom Vander Aa <vanderaa@imec.be>
2019-07-10 09:03:42 +0200

    pass cmdline pararms

--- vms/data/chem2vec/fpga-zcu102/run.sh
+++ vms/data/chem2vec/fpga-zcu102/run.sh
@@ -6,14 +6,14 @@ set -e
 cmp BOOT.BIN /boot/BOOT.BIN || {
     sudo cp BOOT.BIN /boot/
     echo "Press any key to reboot"
-    read
+    read a
     sudo reboot
 }
 
-export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/performance
+export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/debug
 export XTASKS_CONFIG_FILE=predict.xtasks.config
 export NX_ARGS="--fpga-enable --verbose"
 #export NX_ARGS="--instrumentation=extrae"
 export EXTRAE_CONFIG_FILE=extrae.xml
 
-./predict-i
+./predict-i $*
