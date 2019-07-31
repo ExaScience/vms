5fca62eafc813246457e0ea3823e0842d488150d
Tom Vander Aa <vanderaa@imec.be>
2019-07-31 15:41:27 +0200

    NX not verbose

--- vms/data/chem2vec/fpga-zcu102/run.sh
+++ vms/data/chem2vec/fpga-zcu102/run.sh
@@ -12,7 +12,7 @@ cmp BOOT.BIN /boot/BOOT.BIN || {
 
 export LD_LIBRARY_PATH=/opt/install-arm64/nanox/lib/debug
 export XTASKS_CONFIG_FILE=predict.xtasks.config
-export NX_ARGS="--fpga-enable --verbose"
+#export NX_ARGS="--fpga-enable --verbose"
 #export NX_ARGS="--instrumentation=extrae"
 export EXTRAE_CONFIG_FILE=extrae.xml
 
