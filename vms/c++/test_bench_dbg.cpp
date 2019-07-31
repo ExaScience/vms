b1a4e8d0307394245fc3827900e350a3a3e09dbb
Tom Vander Aa <vanderaa@imec.be>
2019-07-31 15:40:46 +0200

    Update debug test_bench

--- vms/c++/test_bench_dbg.cpp
+++ vms/c++/test_bench_dbg.cpp
@@ -17,42 +17,32 @@ double tick() {
     return (double)clock() / CLOCKS_PER_SEC;
 }
 
-const int feat_limit = 1;
-const int comp_limit = 1;
-
 void prepare_tb_input(
     int num_compounds,
-    const float in[tb_num_compounds][num_features],
-    F_base out[][num_features])
+    const float F_in[tb_num_compounds][num_features],
+    F_base F_out[][num_features])
 {
     for (int c = 0; c < num_compounds; c++)
         for (int p = 0; p < num_features; p++)
         {
-            float i = in[c%tb_num_compounds][p];
-            if (p < feat_limit && c < comp_limit) 
-            {
-                out[c][p] = F_type(i);
-                printf("F[%d][%d]: %.2f (%d)\n", c, p, i, out[c][p]);
-            } else {
-                out[c][p] = 0;
-            }
+            float val =F_in [c%tb_num_compounds][p];
+            F_out[c][p] = F_base(F_type(val));
         }
 }
-
 void prepare_model(
     const float U_in[num_samples][num_proteins][num_latent],
-    const float mu_in[num_samples][num_latent],
+    const float M_in[num_samples][num_latent],
     const float B_in[num_samples][num_features][num_latent],
     U_base U_out[num_samples][num_proteins][num_latent],
-    mu_base mu_out[num_samples][num_latent],
+    M_base M_out[num_samples][num_latent],
     B_base B_out[num_samples][num_features][num_latent],
     P_base &U_check,
-    P_base &mu_check,
+    P_base &M_check,
     P_base &B_check
     )
 {
 	CRC_INIT(U_check);
-	CRC_INIT(mu_check);
+	CRC_INIT(M_check);
 	CRC_INIT(B_check);
 
     for (int i = 0; i < num_samples; i++)
@@ -60,33 +50,31 @@ void prepare_model(
         for (int j = 0; j < num_proteins; j++)
             for (int k = 0; k < num_latent; k++)
             {
-                U_out[i][j][k] = U_base(U_type(U_in[i][j][k]));
+                float val;
+                //val = U[i][j][k];
+                if (j==0 && k<4 && i<1) val = 1.; else val = 0.;
+                U_out[i][j][k] = U_base(U_type(val));
                 CRC_ADD(U_check, U_out[i][j][k]);
             }
 
         for (int j = 0; j < num_latent; j++)
         {
-            mu_out[i][j] = mu_base(mu_type(mu_in[i][j]));
-            CRC_ADD(mu_check, mu_out[i][j]);
+            float val = .0; //M[i][j];
+            M_out[i][j] =  M_base(M_type(val));
+            CRC_ADD(M_check, M_out[i][j]);
         }
 
         for (int j = 0; j < num_features; j++)
             for (int k = 0; k < num_latent; k++)
             {
-                B_out[i][j][k] = B_type(B_in[i][j][k]);
-                if (k == 0 && j < num_features) {
-                    B_type b = B_base(1024*(j+1));
-                    B_out[i][j][k] = b;
-                    float f = (float)b;
-                    printf("B[%d][%d][%d]: %.8f (%d)\n", i, j, k, B_out[i][j][k], f);
-                } else {
-                    B_out[i][j][k] = 0;
-                }
-
+                float val;
+                if (j<4 && k<4 && i<1) val = B[i][j][k]; else val = 0.;
+                B_out[i][j][k] = B_base(B_type(val));
                 CRC_ADD(B_check, B_out[i][j][k]);
             }
     }
 }
+
 
 int check_result(
     int num_compounds,
@@ -94,13 +82,25 @@ int check_result(
     const float ref[tb_num_compounds][num_proteins])
 {
     int nerrors = 0;
-    for (int c = 0; c < comp_limit; c++)
-        for (int p = 0; p < 1; p++)
+    int np = 8;
+    for (int c = 0; c < num_compounds; c++)
+        for (int p = 0; p < np; p++)
         {
             float o = P_type(out[c][p]);
-            printf("at [%d][%d]: %f (%d) (*8.333=%.2f)\n", c, p, o, out[c][p], o*8.333);
+            float r = ref[c % tb_num_compounds][p];
+            if (std::abs(o - r) < epsilon || o == .0)
+            {
+                //printf("ok at [%d][%d]: %f == %f\n", c, p, o, r);
+            }
+            else
+            {
+                printf("error at [%d][%d]: %f != %f\n", c, p, o, r);
+                printf("         [%d][%d]: %d != %d\n", c, p, out[c][p], P_base(P_type(r)));
+                nerrors++;
+            }
         }
 
+    printf("%d errors (out of %d)\n", nerrors, num_compounds * num_proteins);
     return nerrors;
 }
 
@@ -119,8 +119,6 @@ int main(int argc, char *argv[])
         num_compounds = std::atoi(argv[2]);
     }
     
-    std::cout << typeid(P_type).name() << std::endl;
-
     printf("  dt:    %s\n", DT_NAME);
     printf("  nrep:  %d\n", num_repeat);
     printf("  nprot: %d\n", num_proteins);
@@ -130,32 +128,40 @@ int main(int argc, char *argv[])
     printf("  nlat:  %d\n", num_latent);
     printf("  nsmpl: %d\n", num_samples);
 
-    P_base  tb_output_base[num_compounds][num_proteins];
-    F_base  tb_input_base[num_compounds][num_features];
-    U_base  U_base[num_samples][num_proteins][num_latent];
-    mu_base mu_base[num_samples][num_latent];
-    B_base  B_base[num_samples][num_features][num_latent];
-    P_base U_check_tb, mu_check_tb, B_check_tb;
+    P_base (*tb_output_base)[num_proteins] = new P_base[num_compounds][num_proteins];
+    F_base (*tb_input_base)[num_features] = new F_base[num_compounds][num_features];
+
+    U_base (*Ub)[num_proteins][num_latent] = new U_base[num_samples][num_proteins][num_latent];
+    M_base (*Mb)[num_latent]               = new M_base[num_samples][num_latent];
+    B_base (*Bb)[num_features][num_latent] = new B_base[num_samples][num_features][num_latent];
+
+    P_base  U_check_tb, M_check_tb, B_check_tb;
 
     prepare_tb_input(num_compounds, tb_input, tb_input_base);
-    prepare_model(U, mu, B, U_base, mu_base, B_base, U_check_tb, mu_check_tb, B_check_tb);
+    prepare_model(U, M, B, Ub, Mb, Bb, U_check_tb, M_check_tb, B_check_tb);
 
     int nerrors = 0;
 
     printf("Updating model\n");
-    update_model(U_base, mu_base, B_base);
+    update_model(Ub, Mb, Bb);
 
     printf("Predicting\n");
     double start = tick();
     for(int n=0; n<num_repeat; n++)
     {
-        predict_compound(num_compounds, tb_input_base, tb_output_base);
+        predict_compounds(num_compounds, tb_input_base, tb_output_base);
     }
 #pragma omp taskwait
     double stop = tick();
     nerrors += check_result(num_compounds, tb_output_base, tb_ref);
     double elapsed = stop-start;
     printf("took %.2f sec; %.2f compounds/sec\n", elapsed, num_compounds * num_repeat / elapsed);
+
+    delete[] tb_output_base;
+    delete[] tb_input_base;
+    delete[] Ub;
+    delete[] Mb;
+    delete[] Bb;
 
     return nerrors;
 }
