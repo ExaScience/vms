b9a88b5fe280d514f5aa8bcf97af9f6e6aa0e447
Tom Vander Aa <vanderaa@imec.be>
2019-10-25 11:59:17 +0200

    FIX: remove cflags in vivado hls script

--- vms/codegen/run.tcl
+++ vms/codegen/run.tcl
@@ -1,9 +1,7 @@
 open_project -reset hls
 set_top predict_or_update_model
-set flags "-DT_"
-append flags [lindex $argv 2]
-add_files code/predict.cpp -cflags $flags
-add_files -tb code/test_bench.cpp -cflags $flags
+add_files code/predict.cpp 
+add_files -tb code/test_bench.cpp 
 open_solution "solution1"
 set_part {xczu9eg-ffvb1156-1l-i} -tool vivado
 create_clock -period 5 -name default
