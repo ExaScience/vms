b0eac68bb7a6ff3413910c3ec22cfba8a955d572
Tom Vander Aa <vanderaa@imec.be>
2019-11-12 16:33:53 +0100

    WIP: hls exploration script

--- vms/ci/make_hls.sh
+++ vms/ci/make_hls.sh
@@ -2,26 +2,35 @@
 
 set -x
 
-WORKDIR=$1
-NUM_LATENT=$2
-NUM_SAMPLES=$3
+WORKDIR=$PWD/work
 DATASET=chem2vec
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $SCRIPT_DIR
 GIT_ROOT=$(git rev-parse --show-toplevel)
 GIT_PREFIX=$(git rev-parse --show-prefix)
 
-RUNDIR=${WORKDIR}/${DATASET}_${NUM_LATENT}_${NUM_SAMPLES}_$(date +%Y%m%d_%H%M)
-mkdir -p ${RUNDIR}
-cd $RUNDIR
+for NUM_LATENT in 8 16 32 
+do
+    for NUM_SAMPLES in 8 16 32
+    do
+        cd $WORKDIR
 
-cp -r $GIT_ROOT/vms/data/$DATASET/* ./
+        RUNDIR=${WORKDIR}/${DATASET}_${NUM_LATENT}_${NUM_SAMPLES}_$(date +%Y%m%d_%H%M)
+        mkdir -p ${RUNDIR}
+        cd $RUNDIR
 
-sed -i -E "s/num_latent = [0-9]+/num_latent = $NUM_LATENT/g" options.ini
-sed -i -E "s/nsamples = [0-9]+/nsamples = $NUM_LATENT/g" options.ini
+        cp -r $GIT_ROOT/vms/data/$DATASET/* ./
 
-make model
-cd hls
+        sed -i -E "s/num_latent = [0-9]+/num_latent = $NUM_LATENT/g" options.ini
+        sed -i -E "s/nsamples = [0-9]+/nsamples = $NUM_LATENT/g" options.ini
+
+        cat <<EOF >run.sh
+        make model
+        cd hls
+        ./make.sh
+EOF
+
+    done
+done
+
 
 
