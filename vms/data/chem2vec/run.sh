279b8cb8f97c0c774568c143e8af3e534abb9ff0
Tom Vander Aa <vanderaa@imec.be>
2019-12-23 13:10:33 +0100

    Upgrade to autoVivado 1.4.2

--- vms/data/chem2vec/run.sh
+++ vms/data/chem2vec/run.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 
+ml autoVivado/1.2.3
+
 DATE=$(date +%Y%m%d_%H%M)
 exec  >stdout_$DATE.log 2>stderr_$DATE.log
 
