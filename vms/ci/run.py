779eca75b4df6e2b92805af19b46d4530a8aff0a
Tom Vander Aa <vanderaa@imec.be>
2020-07-23 10:20:41 +0200

    WIP: ci script

--- vms/ci/run.py
+++ vms/ci/run.py
@@ -6,6 +6,7 @@ import subprocess
 import difflib
 import logging
 import datetime
+import itertools
 
 parameter_space = {
     "datasets" : [ "chem2vec" ],
@@ -18,27 +19,38 @@ parameter_space = {
 git_repo = "/home/vanderaa/euroexa/eurosmurff"
 basedir = "/scratch/vms/ci_" + datetime.datetime.today().strftime("%Y%m%d-%H%M%S")
 
-logging.basicConfig(level = logging.INFO)
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level = logging.INFO, format = log_format)
 
-def execute(cmd, modules = None):
+def execute(cmd, modules = None, workdir = None, log_name = None):
+    if log_name is None:
+        log_name = cmd.translate(str.maketrans(" .", "__")) + ".log"
+
     if modules is not None:
         modules_str = " ".join(modules)
         logging.info("Loading modules \"%s\"", modules_str)
         cmd = "module load " + modules_str + " && " + cmd
 
-    logging.info("$ %s", cmd)
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT, executable='/bin/bash')
+    try:
+        owd = os.getcwd()
+        if workdir is not None:
+            os.chdir(workdir)
 
-    logging.info("%s", result.stdout.decode("utf-8"))
-    if result.returncode != 0:
-        logging.error("Command \"%s\" exited with code %d", cmd, result.returncode)
-        raise subprocess.CalledProcessError(result.returncode, cmd)
-    else:
-        logging.info("Command \"%s\" exited with code %d", cmd, result.returncode)
+        logging.info("Command: \"%s\" in dir \"%s\"", cmd, os.getcwd())
+        with open(log_name, "w") as log_file:
+            result = subprocess.run(cmd, shell=True, stdout=log_file, stderr=subprocess.STDOUT, executable='/bin/bash')
 
+        if result.returncode != 0:
+            logging.error("Command \"%s\" exited with code %d", cmd, result.returncode)
+            raise subprocess.CalledProcessError(result.returncode, cmd)
+        else:
+            logging.info("Command \"%s\" exited with code %d", cmd, result.returncode)
+
+    finally:
+        os.chdir(owd)
 
 def patch_file(fname, pattern, repl):
-    logging.info("patching %s: %s -> %s ", fname, pattern, repl)
+    logging.info("patching %s: %s -> %s ", fname, repr(pattern), repr(repl))
 
     with open(fname, "r") as f:
         content = f.read()
@@ -50,14 +62,22 @@ def patch_file(fname, pattern, repl):
         f.write(new_content)
 
 
-class FPGATask:
-    def run(self, dataset, num_latent, num_samples, datatype):
-        newdir = "%s/%s_%d_%d_%s" % (basedir, dataset, num_latent, num_samples, datatype)
+def run(dataset, num_latent, num_samples, datatype):
+
+    # change dir and log to file
+    newdir = "%s/%s_%d_%d_%s" % (basedir, dataset, num_latent, num_samples, datatype)
+    os.makedirs(newdir)
+    os.chdir(newdir)
+    file_logger = logging.FileHandler('ci.log')
+    file_logger.setFormatter(logging.Formatter(log_format))
+    file_logger = logging.getLogger().addHandler(file_logger)
+
+    try:
+        logging.info("Starting dataset=%s num_latent=%d num_samples=%d datatype=%s in %s",
+            dataset, num_latent, num_samples, datatype, os.getcwd())
 
         # checkout 
-        os.makedirs(newdir)
-        os.chdir(newdir)
-        execute("git clone %s repo" % git_repo)
+        execute("git clone %s repo" % git_repo, log_name = "clone.log")
         os.chdir("repo/vms/")
 
         # patch
@@ -69,14 +89,27 @@ class FPGATask:
         os.chdir("data/%s" % dataset)
         execute("python make.py")
         execute("make model")
-        execute("cd native && make", modules=["ompss"])
-        execute("cd smp-x86 && make")
-        execute("cd smp-arm64 && make", modules=["QEMU",  "petalinux/2016.4", "mcxx-arm64"])
-        execute("make hls", modules = ["Vivado"])
+        execute("make", modules=["ompss"], workdir="native")
+        execute("make", modules=["ompss"], workdir="smp-x86")
+        execute("make", modules=["QEMU",  "petalinux/2016.4", "mcxx-arm64"], workdir="smp-arm64")
+        execute("make hls", modules = ["Vivado/2017.4"])
+        execute("make bitstream", modules = ["autoVivado"], workdir="fpga-zcu102")
+    
+    except KeyboardInterrupt: 
+        logging.info("Caught Ctrl-C, aborting")
+        raise
 
+    except Exception as e:
+        logging.error("Failed with %s: %s", type(e), e)
+        pass
 
+    finally:
+        logging.getLogger().removeHandler(file_logger)
 
-
-
-FPGATask().run("chem2vec", 11, 16, "float")
-
+if __name__ == "__main__":
+    import argparse
+    
+    for params in itertools.product(*parameter_space.values()):
+        logging.info("Current parameter set: %s", list(zip(parameter_space.keys(), params)))
+        run(*params)
+    
