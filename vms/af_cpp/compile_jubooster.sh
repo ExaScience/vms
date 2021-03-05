81e5512e25fb268e6f744741406376af8813c5f4
Tom Vander Aa <vanderaa@imec.be>
2021-03-05 14:15:15 +0100

    Compile af_cpp with MKL

--- vms/af_cpp/compile_jubooster.sh
+++ vms/af_cpp/compile_jubooster.sh
@@ -6,8 +6,7 @@ set -x
 
 g++ -fopenmp -O3 -g -std=c++11 af_predict.cpp io.cpp -o af_predict \
     -I$CONDA_PREFIX/include/eigen3 -I$CONDA_PREFIX/include/ \
-    -Wl,-rpath,$CONDA_PREFIX/lib64 -L$CONDA_PREFIX/lib64 \
     -Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib \
     -lafcuda -lboost_program_options \
-    -lcudart # -lcublas -lcusolver -lcufft -lnvrtc -lcusparse -lforge -lXev
-    #-DCUDA_PROFILE -I/usr/local/cuda/include -L/usr/local/cuda/lib64 \
+    -DEIGEN_USE_MKL_ALL \
+    -lmkl_rt
