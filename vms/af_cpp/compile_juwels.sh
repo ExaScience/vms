c901e724333c58c131315c63b5e1beea15147afa
Tom Vander Aa <vanderaa@imec.be>
2021-03-05 15:26:34 +0100

    amd and intel

---
+++ vms/af_cpp/compile_juwels.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+module load CUDA 
+
+set -x
+
+g++ -fopenmp -O3 -g -std=c++11 af_predict.cpp io.cpp -o af_predict \
+    -I$CONDA_PREFIX/include/eigen3 -I$CONDA_PREFIX/include/ \
+    -Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib \
+    -lafcpu -lboost_program_options \
+    -DEIGEN_USE_MKL_ALL \
+    -lmkl_rt
---
+++ vms/af_cpp/fake_intel.c
@@ -0,0 +1,7 @@
+/*
+ * $ gcc -shared -fPIC -o libfakeintel.so fakeintel.c
+ */
+
+int mkl_serv_intel_cpu_true() {
+	  return 1;
+}
