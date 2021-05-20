181c9f578fd0e8d50ef9e16810a2dafebb9476da
Tom Vander Aa <vanderaa@imec.be>
2021-05-20 10:53:01 +0200

    WIP: coka compile script

---
+++ vms/af_cpp/compile_coka.sh
@@ -0,0 +1,21 @@
+set -x
+
+
+if [ -n "$CONDA_PREFIX" ]
+then
+
+icpc -fopenmp -march=native -O3 -g -std=c++11 af_predict.cpp io.cpp -o af_predict \
+    -DEIGEN_DONT_PARALLELIZE -DEIGEN_USE_MKL_ALL \
+    -I$CONDA_PREFIX/include/eigen3 -I$CONDA_PREFIX/include/ \
+    -Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib \
+    -lafcuda -lboost_program_options -lmkl_rt 
+
+#    -DEIGEN_USE_MKL_ALL \
+# -lmkl_intel_lp64
+
+else
+
+g++ -fopenmp -O2 -g -std=c++11 af_predict.cpp io.cpp -o af_predict -I/usr/local/include/eigen3 -I/opt/arrayfire/include/ \
+    -Wl,-rpath,/opt/arrayfire/lib64 -L/opt/arrayfire/lib64 -laf -lboost_program_options
+
+ fi
