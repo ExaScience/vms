155b1149e7735677eae4dbb3b90569ea208dd755
Tom Vander Aa <vanderaa@imec.be>
2021-06-08 13:38:47 +0200

    WIP: Makefiles

--- vms/makefiles/hls/vivado_hls.tcl
+++ vms/makefiles/hls/vivado_hls.tcl
@@ -1,7 +1,7 @@
 open_project -reset hls
 set_top predict_or_update_model
-add_files code/non_ocl.cpp  -cflags "-std=c++11"
-add_files -tb code/host.cpp -cflags "-std=c++11"
+add_files non_ocl.cpp  -cflags "-std=c++11"
+add_files -tb host.cpp -cflags "-std=c++11"
 open_solution "solution1"
 set_part {xczu9eg-ffvb1156-1l-i} -tool vivado
 create_clock -period 5 -name default
