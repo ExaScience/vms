c2cdd593aad4dfd4410e3a0de580436bcf9842e2
Tom Vander Aa <vanderaa@imec.be>
2021-06-10 13:26:25 +0200

    WIP: cleanup build script

--- vms/scripts/build.py
+++ vms/scripts/build.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python3
 
 import os
-from os import chdir
+import glob
 import shutil
+import difflib
 import re
 import subprocess
 import logging
 import datetime
 import xml.etree.cElementTree as cET
 
-def execute(cmd, modules = None, workdir = None, log_name = None):
-    if log_name is None:
-        log_name = cmd.translate(str.maketrans(" .", "__")) + ".log"
+global_build_logger = None
+LOGFORMAT = '%(asctime)s - %(process)d - %(levelname)s - %(message)s'
 
+def build_logger():
+    global global_build_logger
+    return global_build_logger
+
+def make_build_logger(dir):
+    global global_build_logger
+    global_build_logger = logging.getLogger(os.path.basename(dir))
+    fh = logging.FileHandler('build.log')
+    fh.setFormatter(logging.Formatter(LOGFORMAT))
+    fh.setLevel(logging.INFO)
+    build_logger().addHandler(fh)
+    return build_logger()
+
+def reset_build_logger():
+    global global_build_logger
+    global_build_logger = None
+
+def execute(cmd, modules = None, workdir = None, log_name = "make.log"):
     if modules is not None:
         modules_str = " ".join(modules)
-        logging.info("Loading modules \"%s\"", modules_str)
+        build_logger().info("Loading modules \"%s\"", modules_str)
         cmd = "module load " + modules_str + " && " + cmd
 
     try:
@@ -23,71 +41,75 @@ def execute(cmd, modules = None, workdir = None, log_name = None):
         if workdir is not None:
             os.chdir(workdir)
 
-        logging.info("Command: \"%s\" in dir \"%s\"", cmd, os.getcwd())
+        build_logger().info("Command: \"%s\" in dir \"%s\"", cmd, os.getcwd())
         with open(log_name, "w") as log_file:
             result = subprocess.run(cmd, shell=True, stdout=log_file, stderr=subprocess.STDOUT, executable='/bin/bash')
 
         if result.returncode != 0:
-            logging.error("Command \"%s\" exited with code %d", cmd, result.returncode)
-            raise subprocess.CalledProcessError(result.returncode, cmd)
+            # with open(log_file, "r") as f:
+            #    last_lines = f.readlines()[-10:]
+
+            build_logger().error("Command \"%s\" exited with code %d\n", cmd, result.returncode)
+            raise subprocess.CalledProcessError(result.returncode, f"'{cmd}' in {os.getcwd()}")
         else:
-            logging.info("Command \"%s\" exited with code %d", cmd, result.returncode)
+            build_logger().info("Command \"%s\" exited with code %d", cmd, result.returncode)
 
     finally:
         os.chdir(owd)
 
 def patch_file(fname, pattern, repl):
-    logging.info("patching %s: %s -> %s ", fname, repr(pattern), repr(repl))
+    build_logger().info("patching %s: %s -> %s ", fname, repr(pattern), repr(repl))
 
     with open(fname, "r") as f:
         content = f.read()
         new_content = re.sub(pattern, repl, content)
        
-    logging.debug("  diff: %s", difflib.ndiff(content, new_content))
+    build_logger().debug("  diff: %s", difflib.ndiff(content, new_content))
         
     with open(fname, "w") as f:
         f.write(new_content)
 
 def resource_utilization(name):
-    report_file = f'hls/solution1/syn/report/{name}_csynth.xml'
+    report_file, = glob.glob(f'**/{name}_csynth.xml', recursive=True)
     root = cET.parse(report_file).getroot()
     area = root.find('AreaEstimates')
 
     ar = { r.tag : int(r.text) for r in area.find('AvailableResources') }
     ur = { r.tag : int(r.text) for r in area.find('Resources') }
     merged = { t : ( ar[t], ur[t] ) for t in ar.keys() }
-    logging.info("Resource utilization of %s:\n%s", name, merged)
+    build_logger().info("Resource utilization of %s:\n%s", name, merged)
 
     return merged
 
 def latency_estimation(name):
-    report_file = f'hls/solution1/syn/report/{name}_csynth.xml'
+    report_file, = glob.glob(f'**/{name}_csynth.xml', recursive=True)
     root = cET.parse(report_file).getroot()
     latency_node = root.find('PerformanceEstimates/SummaryOfOverallLatency/Average-caseLatency')
     latency = int(latency_node.text)
-    logging.info("Average latency of %s: %d", name, latency)
+    build_logger().info("Average latency of %s: %d", name, latency)
     return latency
 
 def log_xtasks_config():
     with open ("fpga-zcu102/predict.xtasks.config", "r") as f:
-        logging.info("predict.xtasks.config:\n%s", f.read())
+        build_logger().info("predict.xtasks.config:\n%s", f.read())
 
-def populate_dir(source_dir, build_dir, dataset, num_latent, num_samples, datatype):
-    if build_dir is None:
+def action_populate(sourcedir, basedir, dataset, num_latent, num_samples, datatype):
+    if basedir is None:
         basedir = "ci_" + datetime.datetime.today().strftime("%Y%m%d-%H%M%S")
-        build_dir = os.path.join(basedir, "%s_%d_%d_%s" % (dataset, num_latent, num_samples, datatype))
 
-    if os.path.isdir(build_dir):
-        logging.warning("Not populating %s: already exists", build_dir)
-        return
+    builddir = os.path.join(basedir, "%s_%d_%d_%s" % (dataset, num_latent, num_samples, datatype))
 
-    makefiles_dir = os.path.abspath(os.path.join(source_dir, "makefiles"))
-    shutil.copytree(makefiles_dir, build_dir, copy_function=os.symlink)
+    if os.path.isdir(builddir):
+        logging.warning("Not populating %s: already exists", builddir)
+        return builddir
+
+    makefiles_dir = os.path.abspath(os.path.join(sourcedir, "makefiles"))
+    shutil.copytree(makefiles_dir, builddir, copy_function=os.symlink)
 
     timestamp = datetime.datetime.now()
-    abs_source_dir = os.path.abspath(source_dir)
-    abs_build_dir = os.path.abspath(build_dir)
-    with open(os.path.join(build_dir, "config.mk"), "w") as config_file:
+    abs_source_dir = os.path.abspath(sourcedir)
+    abs_build_dir = os.path.abspath(builddir)
+    with open(os.path.join(builddir, "config.mk"), "w") as config_file:
         config_file.write(
             f"""# generated on {timestamp}
 SRCDIR = {abs_source_dir}
@@ -98,39 +120,39 @@ NUM_SAMPLES = {num_samples}
 DATATYPE = {datatype}
 """)
 
-    logging.info("Populated %s", build_dir)
+    logging.info("Populated %s", builddir)
+    return builddir
 
-def build_dir(dir):
+def action_build(dir):
     os.chdir(dir)
-    file_logger = logging.FileHandler('build.log')
-    file_logger.setFormatter(logging.Formatter(log_format))
-    logging.getLogger().addHandler(file_logger)
+    absdir = os.getcwd()
+    make_build_logger(dir)
 
     try:
-        logging.info("Building %s", dir)
+        build_logger().info("Building %s", dir)
 
         execute("make model")
         execute("make", modules=["Vitis/2020.1"], workdir="native")
-        execute("make", modules=["ompss"], workdir="smp-x86")
-        execute("make", modules=["QEMU",  "petalinux/2016.4", "mcxx-arm64"], workdir="smp-arm64")
+        execute("make", modules=["gcc-7.3.0-gcc-9.3.0-mk2k4ie" ,"ompss-19.06-gcc-7.3.0-jseith3"], workdir="smp-x86")
+        execute(f"dockfpga -w {absdir} make", workdir="smp-arm64")
 
-        execute("make hls", modules = ["Vivado/2017.4"])
+        execute("make hls", modules = ["Vitis/2020.1"])
         resource_utilization("predict_or_update_model")
-        latency_estimation("dataflow_in_loop")
+        latency_estimation("predict_one_compound")
 
-        execute("make bitstream", modules = ["autoVivado"], workdir="fpga-zcu102")
+        execute(f"dockfpga -w {absdir} make bitstream", workdir="fpga-zcu102")
         log_xtasks_config()
     
     except KeyboardInterrupt: 
-        logging.info("Caught Ctrl-C, aborting")
+        build_logger().info("Caught Ctrl-C, aborting")
         raise
 
     except Exception as e:
-        logging.error("Failed with %s: %s", type(e), e)
+        build_logger().error("Failed with %s: %s", type(e), e)
         return False
 
     finally:
-        logging.getLogger().removeHandler(file_logger)
+        reset_build_logger()
 
     print("Done ...")
     return True
@@ -140,21 +162,26 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--build-dir", type=str, metavar="DIR", default = None, help="Build directory")
+    parser.add_argument("--builddir", type=str, metavar="DIR", default = None, help="Actual build directory")
+    parser.add_argument("--basedir", type=str, metavar="DIR", default = None, help="Base of builds directory")
     parser.add_argument("--dataset", type=str, default="chem2vec", choices=["chem2vec", "random", ], help="Dataset to use")
     parser.add_argument("--datatype", type=str, default="fixed", choices=["float", "fixed", "half", "mixed"], help="Internal data-type")
     parser.add_argument("--num-latent", type=int, default=4, help="Number of latent dimensions")
     parser.add_argument("--num-samples", type=int, default=4, help="Number of samples to collect")
-    parser.add_argument("--build", action="store_true", help="Also build")
+    parser.add_argument("--do-build", action="store_true", help="Do build directory")
+    parser.add_argument("--verbose", action="store_true", help="Verbose logging on stderr")
     args = parser.parse_args()
 
-    log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level = logging.INFO, format = log_format)
+    if args.verbose:
+        logging.basicConfig(format=LOGFORMAT, level=logging.INFO)
 
-    populate_dir('.', args.build_dir, args.dataset, args.num_latent, args.num_samples, args.datatype)
+    if args.builddir is None:
+        builddir = action_populate('.', args.basedir, args.dataset, args.num_latent, args.num_samples, args.datatype)
+    else:
+        builddir = args.builddir
 
-    if args.build:
-        build_dir(args.build_dir)
+    if args.do_build:
+        action_build(builddir)
     
 
 
