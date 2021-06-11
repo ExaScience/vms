b4b26d5c82b2c93fc766b2a8be72da7e4b8e7834
Tom Vander Aa <vanderaa@imec.be>
2021-06-11 15:26:28 +0200

    HLS: switch to crdb

--- vms/makefiles/hls/vivado_hls.tcl
+++ vms/makefiles/hls/vivado_hls.tcl
@@ -3,7 +3,10 @@ set_top predict_or_update_model
 add_files non_ocl.cpp  -cflags "-std=c++11"
 add_files -tb host.cpp -cflags "-std=c++11"
 open_solution "solution1"
-set_part {xczu9eg-ffvb1156-1l-i} -tool vivado
+# zcu102
+# set_part {xczu9eg-ffvb1156-1l-i} -tool vivado
+# crdb
+set_part {xcvu9p-fsgd2104-2-i} -tool vivado
 create_clock -period 5 -name default
 csim_design -compiler clang
 csynth_design
