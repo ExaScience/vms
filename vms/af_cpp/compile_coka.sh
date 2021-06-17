1ac4687ec18f796090cbc078ac5486a2f075867e
Tom Vander Aa <vanderaa@imec.be>
2021-06-17 11:08:38 +0200

    Added lib64/ in compile scripts

--- vms/af_cpp/compile_coka.sh
+++ vms/af_cpp/compile_coka.sh
@@ -8,6 +8,7 @@ icpc -fopenmp -march=native -O3 -g -std=c++11 af_predict.cpp io.cpp -o af_predic
     -DEIGEN_DONT_PARALLELIZE -DEIGEN_USE_MKL_ALL \
     -I$CONDA_PREFIX/include/eigen3 -I$CONDA_PREFIX/include/ \
     -Wl,-rpath,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib \
+    -Wl,-rpath,$CONDA_PREFIX/lib64 -L$CONDA_PREFIX/lib64 \
     -lafcuda -lboost_program_options -lmkl_rt 
 
 #    -DEIGEN_USE_MKL_ALL \
