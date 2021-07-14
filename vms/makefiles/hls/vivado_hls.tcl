9a8f8363edac29efd12118c448a8e58041aa991a
Tom Vander Aa <vanderaa@imec.be>
2021-07-14 16:39:47 +0200

    non_ocl -> ompss

--- vms/makefiles/hls/vivado_hls.tcl
+++ vms/makefiles/hls/vivado_hls.tcl
@@ -1,6 +1,6 @@
 open_project -reset hls
 set_top predict_one_block
-add_files non_ocl.cpp  -cflags "-std=c++11"
+add_files ompss.cpp  -cflags "-std=c++11"
 add_files -tb main.cpp -cflags "-std=c++11"
 open_solution "solution1"
 # zcu102
