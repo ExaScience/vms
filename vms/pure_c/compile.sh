c7c9c35fdd64c96102e7a0521873a998038b12fe
Tom Vander Aa <vanderaa@imec.be>
2022-02-15 10:37:24 +0100

    WIP: simple compile script

--- vms/pure_c/compile.sh
+++ vms/pure_c/compile.sh
@@ -1,1 +1,18 @@
-mcc -DOMPSS -O3 --ompss-2 predict.c test_bench.c ompss.c mpi.c 
+#!/bin/bash
+
+set -e
+set -x
+
+. /home/vanderaa/local/spack/share/spack/setup-env.sh
+
+spack unload --all
+spack load mcxx ^nanos6@cluster ^intel-oneapi-mpi
+mcc -DOMPSS -O3 --ompss-2 predict.c test_bench.c ompss.c mpi.c -o predict_ompss_cluster
+
+spack unload --all
+spack load mcxx ^nanos6@argodsm ^intel-oneapi-mpi
+mcc -DOMPSS -O3 --ompss-2 predict.c test_bench.c ompss.c mpi.c -o predict_ompss_argodsm
+
+spack unload --all
+spack load intel-oneapi-mpi@2021.5.1%gcc@9.3.0
+gcc -DUSE_MPI -O3 predict.c test_bench.c ompss.c mpi.c -o predict_mpi -lmpi
