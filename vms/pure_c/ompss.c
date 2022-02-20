8d28f0fe8cff107f7c2460f5e54e8311e02c2428
Tom Vander Aa <vanderaa@imec.be>
2022-02-20 16:06:56 +0100

    FIX: always use lmalloc

--- vms/pure_c/ompss.c
+++ vms/pure_c/ompss.c
@@ -8,8 +8,7 @@ void *lmalloc(unsigned long size, int segment_id)
 {
 #ifdef USE_OMPSS
     UNUSED(segment_id);
-    if (mpi_world_size == 1) return malloc(size);
-    else return nanos6_lmalloc(size);
+    return nanos6_lmalloc(size);
 #elif defined(USE_GPI)
     return gaspi_malloc(segment_id, size);
 #else
