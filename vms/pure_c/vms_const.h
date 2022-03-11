aa337722b91c2129eae3ae5225f00ed12151e65f
Tom Vander Aa <vanderaa@imec.be>
2022-03-11 15:15:47 +0100

    WIP: larger model

--- vms/pure_c/vms_const.h
+++ vms/pure_c/vms_const.h
@@ -1,6 +1,5 @@
 #pragma once
 #define num_proteins 114
 #define num_features 469
-#define num_latent 16
-#define num_samples 32
-#define log_num_samples 5
+#define num_latent 128
+#define num_samples 128
